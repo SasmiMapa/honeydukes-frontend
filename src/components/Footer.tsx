59b632cf0c3bc5ee605b234384c5eadcf7810db8
SasmiMapa <sasmimapa@gmail.com>
2024-07-17 21:38:39 +0530

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div className="bg-[#ca8a04] py-10">
+      <div className="container max-auto flex flex-col: flex-row justify-between items-center">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          Honeydukes.com
+        </span>
+        <span className="text-white font-bold tracking-tight flex gap-4">
+          <span>Privacy Policy</span>
+          <span>Terms of Service</span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
