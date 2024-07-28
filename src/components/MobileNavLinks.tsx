ab93f8630305adf0fb25b2213c5ffe771ec0f1f6
SasmiMapa <sasmimapa@gmail.com>
2024-07-28 11:03:18 +0530

    commit

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -12,6 +12,12 @@ const MobileNavLinks = () => {
       >
         User Profile
       </Link>
+      <Link
+        to="/manage-restaurant"
+        className="flex bg-white items-center font-bold hover:text-[#ca8a04]"
+      >
+        My Restaurant
+      </Link>
       <Button
         onClick={() => logout()}
         className="flex items-center px-3 font-bold hover:bg-[#ca8a04]"
